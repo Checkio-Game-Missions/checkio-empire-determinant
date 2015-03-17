b5a6d6419b794305b84caf506da233365debff3b
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-17 09:29:23 +0300

    fix golf

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -8,7 +8,7 @@ from tests import TESTS
 class Referee(RefereeCodeGolf):
     TESTS = TESTS
     BASE_POINTS = 25
-    DEFAULT_LENGTH = 250
+    DEFAULT_MAX_CODE_LENGTH = 250
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "golf"
